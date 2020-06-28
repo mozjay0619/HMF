985ab0d94aac02df05966c9df481c3638e9e09e0
Jay Kim <jkim2@grubhub.com>
2020-06-28 17:55:46 -0500

    0.0.b1
    0.0.b1
    
    0.0.b1 travis
    
    0.0.b1 added requirements
    
    0.0.b1 pytest
    
    0.0.b1 tabs
    
    0.0.b1 pytest
    
    0.0.b1

--- HMF/__init__.py
+++ HMF/__init__.py
@@ -1,6 +1,6 @@
 
 
-__version__ = "0.0.b2"
+__version__ = "0.0.b1"
 
 from .core import BaseHMF
 from .parallel import WriterProcessManager
--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 
-import tables
+import HMF
 
-VERSION = tables.__version__
+VERSION = HMF.__version__
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
-	name="tables-m2", 
+	name="HMF", 
 	version=VERSION,
 	author="Jay Kim",
 	description="Hierarchical numpy memmap datasets for Python",
 	long_description=long_description,
 	long_description_content_type="text/x-rst",
-	url="https://github.com/mozjay0619/tables-m2",
+	url="https://github.com/mozjay0619/HMF",
 	license="DSB 3-clause",
 	packages=find_packages(),
 	install_requires=[]
