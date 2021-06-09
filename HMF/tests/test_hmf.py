eaa2d1ec8e19032a8cbc326f32c44961be9a082a
Jay Kim <jkim2@grubhub.com>
2021-06-08 19:38:01 -0500

    0.0.b45

--- HMF/tests/test_hmf.py
+++ HMF/tests/test_hmf.py
@@ -38,171 +38,171 @@ def test_writing_and_reading_arrays():
 	assert np.array_equal(np.arange(3), f.get_array('/group2/array2'))
 	assert np.array_equal(np.arange(3), f.get_array('/group2/subgroup1/array2'))
 
-def test_writing_arrays_from_pandas():
+# def test_writing_arrays_from_pandas():
 
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_array('arrayA', ['b', 'c'])
-	f.register_array('arrayB', ['a', 'b'])
-	f.close()
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_array('arrayA', ['b', 'c'])
+# 	f.register_array('arrayB', ['a', 'b'])
+# 	f.close()
 
-	assert np.array_equal(f.get_array('/a/arrayA'), pdf[pdf['group']=='a'][['b', 'c']].values)
-	assert np.array_equal(f.get_array('/b/arrayB'), pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	assert np.array_equal(f.get_array('/a/arrayA'), pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	assert np.array_equal(f.get_array('/b/arrayB'), pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_writing_and_reading_arrays_from_pandas():
+# def test_writing_and_reading_arrays_from_pandas():
 
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_array('arrayA', ['b', 'c'])
-	f.register_array('arrayB', ['a', 'b'])
-	f.close()
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_array('arrayA', ['b', 'c'])
+# 	f.register_array('arrayB', ['a', 'b'])
+# 	f.close()
 
-	f = HMF.open_file('test_file', mode='r+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='r+', verbose=False)
 
-	assert np.array_equal(f.get_array('/a/arrayA'), pdf[pdf['group']=='a'][['b', 'c']].values)
-	assert np.array_equal(f.get_array('/b/arrayB'), pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	assert np.array_equal(f.get_array('/a/arrayA'), pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	assert np.array_equal(f.get_array('/b/arrayB'), pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_writing_dataframes_from_pandas():
+# def test_writing_dataframes_from_pandas():
 
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
-	f.close()
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.close()
 
-	assert np.array_equal(f.get_array('/a/dataframeA'), pdf[pdf['group']=='a'][['b', 'c']].values)
-	assert np.array_equal(f.get_array('/b/dataframeB'), pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	assert np.array_equal(f.get_array('/a/dataframeA'), pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	assert np.array_equal(f.get_array('/b/dataframeB'), pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_writing_and_reading_dataframes_from_pandas():
+# def test_writing_and_reading_dataframes_from_pandas():
 	
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
-	f.close()
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.close()
 
-	f = HMF.open_file('test_file', mode='r+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='r+', verbose=False)
 
-	assert np.array_equal(f.get_array('/a/dataframeA'), pdf[pdf['group']=='a'][['b', 'c']].values)
-	assert np.array_equal(f.get_array('/b/dataframeB'), pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	assert np.array_equal(f.get_array('/a/dataframeA'), pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	assert np.array_equal(f.get_array('/b/dataframeB'), pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_multi_pdf_writing():
+# def test_multi_pdf_writing():
 	
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
 
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.close()
+# 	f.close()
 
-	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/a/dataframeA'), 
-               pdf[pdf['group']=='a'][['b', 'c']].values)
-	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/b/dataframeB'), 
-               pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/a/dataframeA'), 
+#                pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/b/dataframeB'), 
+#                pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_multi_pdf_writing_reading():
+# def test_multi_pdf_writing_reading():
 	
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
 
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.from_pandas(pdf, groupby='group', orderby='c')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.close()
+# 	f.close()
 
-	f = HMF.open_file('test_file', mode='r+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='r+', verbose=False)
 
-	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/a/dataframeA'), 
-               pdf[pdf['group']=='a'][['b', 'c']].values)
-	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/b/dataframeB'), 
-               pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/a/dataframeA'), 
+#                pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	np.array_equal(f.get_dataframe('/__specialHMF__dataFrameNumber_0/b/dataframeB'), 
+#                pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_multi_pdf_writing_using_group_names():
+# def test_multi_pdf_writing_using_group_names():
 
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
 
-	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupA')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupA')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupB')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupB')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.close()
+# 	f.close()
 
-	np.array_equal(f.get_dataframe('/groupA/a/dataframeA'), 
-               pdf[pdf['group']=='a'][['b', 'c']].values)
-	np.array_equal(f.get_dataframe('/groupB/b/dataframeB'), 
-               pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	np.array_equal(f.get_dataframe('/groupA/a/dataframeA'), 
+#                pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	np.array_equal(f.get_dataframe('/groupB/b/dataframeB'), 
+#                pdf[pdf['group']=='b'][['a', 'b']].values)
 
-def test_multi_pdf_writing_and_reading_using_group_names():
+# def test_multi_pdf_writing_and_reading_using_group_names():
 
-	data = np.arange(10*3).reshape((10, 3))
-	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
-	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
-	pdf['group'] = groups
+# 	data = np.arange(10*3).reshape((10, 3))
+# 	groups = ['a'] * 3 + ['b'] * 3 + ['c'] * 4
+# 	pdf = pd.DataFrame(data=data, columns=['a', 'b', 'c'])
+# 	pdf['group'] = groups
 
-	f = HMF.open_file('test_file', mode='w+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='w+', verbose=False)
 
-	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupA')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupA')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupB')
-	f.register_dataframe('dataframeA', ['b', 'c'])
-	f.register_dataframe('dataframeB', ['a', 'b'])
+# 	f.from_pandas(pdf, groupby='group', orderby='c', group_name='groupB')
+# 	f.register_dataframe('dataframeA', ['b', 'c'])
+# 	f.register_dataframe('dataframeB', ['a', 'b'])
 
-	f.close()
+# 	f.close()
 
-	f = HMF.open_file('test_file', mode='r+', verbose=False)
+# 	f = HMF.open_file('test_file', mode='r+', verbose=False)
 
-	np.array_equal(f.get_dataframe('/groupA/a/dataframeA'), 
-               pdf[pdf['group']=='a'][['b', 'c']].values)
-	np.array_equal(f.get_dataframe('/groupB/b/dataframeB'), 
-               pdf[pdf['group']=='b'][['a', 'b']].values)
+# 	np.array_equal(f.get_dataframe('/groupA/a/dataframeA'), 
+#                pdf[pdf['group']=='a'][['b', 'c']].values)
+# 	np.array_equal(f.get_dataframe('/groupB/b/dataframeB'), 
+#                pdf[pdf['group']=='b'][['a', 'b']].values)
 
